f5328a95cf568096e52e5c6f059f2a804885f494
Unknown <mahi_jfg@rocketmail.com>
2019-02-11 15:36:47 +0330

    services done

--- controller.py
+++ controller.py
@@ -1,6 +1,8 @@
 
 # 3rd party modules
+from flask import make_response, abort
 import pythonDevelopmersTest.model.authoize as authorize
+import pythonDevelopmersTest.model.services as services
 
 
 # login api get username and password and return response
@@ -11,7 +13,6 @@ def login ( userpass ) :
         'status':'',
         'token':''
     }
-
     # call model function to check authorization
     token = authorize.login(userpass)
     if token is None:
@@ -21,6 +22,8 @@ def login ( userpass ) :
         response['token'] = token
     return response
 
+
+# login api get token and return success of fail
 def logout(tokenjson):
     # response body created but not field
     response = {
@@ -32,4 +35,22 @@ def logout(tokenjson):
         response['status'] = 'failed'
     else:
         response['status'] = 'success'
+    return response
+
+
+def query(query):
+    response = {
+        'status':'',
+        'response':''
+    }
+    token = query["token"]
+    request = query["request"]
+
+    if authorize.tokencheck(token) is False:
+        response['status']='failed'
+    else:
+        if request in services.SERVICES.keys() :
+            response['response'] = services.SERVICES[request]()
+            response['status'] = 'success'
+
     return response
---
+++ model/services.py
@@ -0,0 +1,35 @@
+from random import randint
+def weather():
+    weathers = ["صاف و آفتابی",
+                "نیمه ابری",
+                "برفی"]
+    return weathers[randint(0,2)]
+
+
+def time():
+    times = ["اذان ظهر ساعت 12:19",
+             "غروب خورشید ساعت 17:41",
+             "اذان مغرب ساعت 18:00"]
+    return times[randint(0, 2)]
+
+
+def exchangerate():
+    exchangerates = ["دلار آمریکا 11 هزار و 700 تومان",
+                     "یورو 13 هزار و 700 تومان",
+                     "پوند انگلیس 15 هزار و 321 تومان"]
+    return exchangerates[randint(0, 2)]
+
+
+def hafez():
+    hafezs  = ["دل و دینم شد و دلبر به ملامت برخاست",
+               "ما را ز خیال تو چه پروای شراب است",
+               "منم که گوشه میخانه خانقاه من است"]
+    return hafezs[randint(0, 2)]
+
+
+SERVICES={
+    'آب و هوا':weather,
+    'اوقات شرعی':time,
+    'نرخ ارز':exchangerate,
+    'فال حافظ':hafez
+}
